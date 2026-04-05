cda808e197d9191fd695d7c96f79813141799979
Wellbiruel <falecombiruel@gmail.com>
2026-04-05 19:06:11 -0300

    fix: remover dotenv e adicionar SSL

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.on('error', (err) => {
