f1f7a887daa396a8b5f17dcddf2b5c2cd50522ad
Wellbiruel <falecombiruel@gmail.com>
2026-04-06 23:01:58 -0300

    fix: aumentar timeout de conexao

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: 'postgresql://postgres.qijaeudgkcnetlzprtcw:r249713s32jUdXo5@aws-1-us-west-2.pooler.supabase.com:6543/postgres',
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  connectionTimeoutMillis: 10000,
+  idleTimeoutMillis: 30000,
+  max: 10
 });
 
 pool.on('error', (err) => {
